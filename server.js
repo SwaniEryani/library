2782d146d922d296f16df067909f1cd4ae2dbc37
Silwan Aleryani <silwanaleryani@Silwans-iMac.local>
2020-12-29 23:22:12 -0500

    fix db issue

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(expressLayouts);
 app.use(express.static("public"));
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/library", { useNewUrlParser: true,
+require('dotenv').config();
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true,
 useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
